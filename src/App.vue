75d8369abbbac91b64adde2e7d5b40e3eb547c71
Ben Hong <ben@bencodezen.io>
2021-06-08 15:08:32 -0400

    switch to vue cli and add netlify init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Hello Twitch!</h1>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+<script>
+export default {}
 </script>
 
 <style>
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+const DATABASE_ID = '9f4656aa97c8413baf78c74697e62ee4'
